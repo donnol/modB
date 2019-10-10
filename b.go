006fefedea5aa021c567e9b883f459559152ff94
liujiandong <liujiandong@xiao100.com>
2019-10-10 14:54:56 +0800

    依赖modC

--- b.go
+++ b.go
@@ -1,3 +1,13 @@
 package modB
 
+import (
+	"fmt"
+
+	"github.com/donnol/modC"
+)
+
+func init() {
+	fmt.Println(modC.C)
+}
+
 var B = "b"
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/donnol/modB
 
 go 1.13
+
+require github.com/donnol/modC v0.2.0
